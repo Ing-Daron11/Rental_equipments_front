da6173293155265717ceadbb0caf58af73e26cfd
Ing.Daron11 <daronmercadog@gmail.com>
2025-06-09 20:50:21 -0500

    final commit

--- tests_e2e/example.spec.ts
+++ tests_e2e/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage loads', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/https://rentalequipmentsfront-production.up.railway.app/');
   await expect(page).toHaveTitle(/./);
 });
